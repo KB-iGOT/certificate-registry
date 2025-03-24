63d12c4f137ec6ed79a616da64e6b117f8d8ed6b
sahilchaudhary <sahil.chaudhary@tarento.com>
2025-03-24 14:17:05 +0530

    Revert "Adding loggers"
    This reverts commit a6822aac622977a83ec21e1774737aacbca5f656.

--- all-actors/src/main/java/org/sunbird/serviceimpl/CertsServiceImpl.java
+++ all-actors/src/main/java/org/sunbird/serviceimpl/CertsServiceImpl.java
@@ -79,7 +79,6 @@ public class CertsServiceImpl implements ICertService {
 
     @Override
     public String addV3(Request request, ActorRef certBackgroundActorRef) throws BaseException {
-        logger.info("This is v3" + request.getRequest());
         Map<String,Object> reqMap = request.getRequest();
         if(isPresentRecipientIdAndCertId(request)){
             validateCertAndRecipientId(reqMap);
@@ -146,10 +145,9 @@ public class CertsServiceImpl implements ICertService {
         Map<String,Object>recordMap= requestMapper.convertValue(certificate,Map.class);
         return CertificateUtil.insertRecord(recordMap, certBackgroundActorRef);
     }
-
     private Response processRecordV2(Map<String, Object> certReqAddMap, String version, ActorRef certBackgroundActorRef) throws BaseException {
-        CertificateV2 certificate = getCertificateV2(certReqAddMap);
-        Map<String, Object> recordMap = requestMapper.convertValue(certificate, Map.class);
+        CertificateV2 certificate=getCertificateV2(certReqAddMap);
+        Map<String,Object>recordMap= requestMapper.convertValue(certificate,Map.class);
         return CertificateUtil.insertRecordV2(recordMap, certBackgroundActorRef);
     }
     private Certificate getCertificate(Map<String, Object> certReqAddMap) {
